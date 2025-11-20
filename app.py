cdeadb5962d674398a6b0a3d5926dde7c3d14994
Su-3214 <2301026@s.asojuku.ac.jp>
2025-11-20 14:40:14 +0900

    update_var.1.0

--- app.py
+++ app.py
@@ -10,9 +10,9 @@ model = genai.GenerativeModel("gemini-flash-latest")
 # --- UI部分 ---
 st.title("みんなでつくるAI翻訳アプリ") # タイトル
 
-source_text = st.text_area("翻訳したいテキストを入力してください") # テキスト入力欄
-target_lang = st.selectbox("翻訳先の言語", []) # セレクトボックス
-submit_button = st.button("〇〇〇") # ボタン
+source_text = st.text_area("最高級のリンゴ") # テキスト入力欄
+target_lang = st.selectbox("台湾語", []) # セレクトボックス
+submit_button = st.button("Let's Translate!") # ボタン
 
 # --- ボタンが押された後の処理 ---
 if submit_button and source_text:
